6b5a74169401ea8ca1b9ea7103a0d4358829856b
kartik <kartikbarade@gmail.com>
2026-09-14 00:04:15 +0530

    Changes the list file

--- Python_Fundamentals/8)Lists_Program.py
+++ Python_Fundamentals/8)Lists_Program.py
@@ -48,3 +48,12 @@ print(numbers[:3])
 print(numbers[2:])
 print(numbers[0:5:2])
 print(numbers[::-1]) # reverse list using slicing
+
+# 3) Accessing nested list
+numbers = [[10,20],[30,40],[50,60]]
+print(numbers[1])
+print(numbers[2][0])
+
+# 4) using len() function
+numbers = [10,20,30,40,50]
+print(len(numbers))
