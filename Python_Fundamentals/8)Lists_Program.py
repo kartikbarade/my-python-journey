bd709a872700291669af76dbe232db91b5ad4deb
kartik <kartikbarade@gmail.com>
2026-09-13 22:35:11 +0530

    Changes the list file

--- Python_Fundamentals/8)Lists_Program.py
+++ Python_Fundamentals/8)Lists_Program.py
@@ -1,8 +1,24 @@
 """
 -------------------Lists-----------------------
-
+A List in python is a collection data type used to store multiple values/elements in a single
+variable.
+Lists are one of the most commonly used data structures in python.
+A list is an ordered,mutable collection of elements enclosed in square brackets [].
 
 """
 # simple example
 fruits = ["Apple","Banana","Mango"]
 print(fruits)
+# Ordered lists
+number = [10,20,30,40]
+print(number)
+# mutable lists
+numbers = [10,20,30,40,50]
+numbers [2] = 70
+print(numbers)
+# Duplicate Values
+numbers = [10,20,10,30,40,20]
+print(numbers)
+# Allows different data types
+data = [10,"Python",3.14,True]
+print(data)
