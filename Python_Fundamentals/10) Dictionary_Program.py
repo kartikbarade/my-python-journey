1e8b7f7b478f9fad0609e90ab75703acaa1cf200
Kartik Barade <kartikbarade@gmail.com>
2026-09-18 19:36:07 +0530

    Create a new Dictionary file

---
+++ Python_Fundamentals/10) Dictionary_Program.py
@@ -0,0 +1,12 @@
+""" 
+                     Dictionary 
+A dictionary in Python is a built-in data structure used to store data 
+in the form of key-value pairs.
+Each key is unique and is used to access its corresponding value. Dictionaries are mutable, 
+meaning we can add, update, and delete items after creating them.
+"""
+student = {
+      "name" : "Kartik",
+      "age" : 21,
+      "branch" : "AIML"
+}
