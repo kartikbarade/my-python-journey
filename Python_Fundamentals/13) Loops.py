5d2a535c490f49104eece67b6f866c4a70003048
Kartik Barade <kartikbarade@gmail.com>
2026-09-24 19:57:21 +0530

    add code in loop file

--- Python_Fundamentals/13) Loops.py
+++ Python_Fundamentals/13) Loops.py
@@ -32,7 +32,7 @@ while i<=5:
     print(i)
     i+=1
 
-# example 
+# Example ----
 names = ['kartik', 'Omkar', 'Suyash', 'Shreya', 'Satish', 'Rudra', 'Dnyaneshwari', 'Gayatri']
 for i in names:
     print(i)
@@ -42,3 +42,64 @@ total = 0
 for i in range(1,11):
     total = total+i
 print(total)
+
+#----------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------
+# for loop
+"""
+              for loop
+A for loop is used to repeat a block of code for each item in a sequence or iterable.
+It is commonly used with :
+            range(), lists,tuples,sets,dictionaries.
+
+- Syntax : for variable in sequence
+               statements
+"""
+# Examples -----
+for i in range(1,6):
+    print(i)
+
+num = int(input("Enter a number"))  # user define number
+for i in range(num):
+    print(i)
+
+fruits = ['Mango', 'Apple', 'Banana', 'Grapes']  # Lists
+for i in fruits: 
+    print(i)
+
+fruits = ['Mango', 'Apple', 'Banana', 'Grapes'] 
+fruits[1]='Orange'
+for i in fruits: 
+    print(i)
+
+fruits = ['Mango', 'Apple', 'Banana', 'Grapes']  
+fruits.append('Kiwi')
+for i in fruits:
+    print(i)
+
+fruits = ['Mango', 'Apple', 'Banana', 'Grapes']  
+fruits.insert(2,'Watermelon')
+for i in fruits:
+    print(i)
+
+fruits = ['Mango', 'Apple', 'Banana', 'Grapes']  
+fruits.pop(2)
+for i in fruits:
+    print(i)
+
+name = "Kartik"  # String
+for i in name:
+    print(i)
+
+student_data = ("kartik", 101,"AIML","Pune",9.14) # Tuple
+for i in student_data:
+    print(i)
+
+branch = {'Comp', 'IT', 'AIML', 'AIDS', 'Mech', 'Elec'}  #Sets
+for i in branch:
+    print(i)
+
+student = {"name":"kartik","rollno":13,"branch":"AIML"}  # Dictionaries
+for key,value in student.items():
+    print(f"{key},{value}")
+
